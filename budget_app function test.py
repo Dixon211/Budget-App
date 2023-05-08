2409501ce549b96642f9bf3baed3ad58807dae77
Dixon211 <131546168+Dixon211@users.noreply.github.com>
2023-05-08 13:09:05 -0400

    Update budget_app function test.py
    finished the function, it now properly gets the rounded down percentage.
    
    The words list is now turned into touples, padded correctly, and added to the category_block variable
    
    corrected the issue on the line editing if statement, needed to append other lists appropriatly

--- budget_app function test.py
+++ budget_app function test.py
@@ -1,3 +1,4 @@
+import math
 class Category:
     def __init__(self, name):
         self.name = name
@@ -42,8 +43,8 @@ class Category:
         else:
             return False
 def create_spend_chart(categories = []):
-    #this string is like adding 2 actions at once. it creates a list of the elements that make up an element with list() and then does a for loop to do it to all elements in the categories list. This turns all the category names into lists of their letters and puts that into the letters list
-    letters = [list(category.name) for category in categories]
+    #this string is like adding 2 actions at once. it creates a list of the elements that make up an element with list() and then does a for loop to do it to all elements in the categories list. This turns all the category names into lists of their words and puts that into the words list
+    words = [list(category.name) for category in categories]
     
     totals = []
     for category in categories:
@@ -52,18 +53,26 @@ def create_spend_chart(categories = []):
             if entry['amount'] < 0:
                 total += abs(entry['amount'])
         totals.append(total)
+    total_sum = sum(totals)
 
-    
+    #this finds the raw percentage then rounds to the nearest 10 by rounding the number/10 and then multiplying by 10, math.floor always rounds down
     percentages = []
     for total in totals:
-        percentage = round(total/ sum(totals), 2) * 100
-        percentages.append(percentage)
+        percentage = round((total/total_sum) * 100)
+        rounded_percentage = math.floor(percentage/10) * 10
+        percentages.append(rounded_percentage)
 
-    max_len = max(len(letters) for letter in letters)
-    equal_letter = []
-    for letter in letters:
-        padded_letter = letter + [" "] * (max_len - len(letter))
-        equal_letter.append(padded_letter)
+
+    max_len = max(len(word_nopad) for word_nopad  in words)
+    # this is a nested statement, a good example of a python one liner it does 2 things. adds "  " to the letters present in the inner lists
+    # and then adds "   " to the outer lists equal to the difference in the max length to the words in the current list. This a feel good line
+    padded_list = [[letter + "  " for letter in word_nopad] + ["   "] * (max_len - len(word_nopad)) for word_nopad in words]
+    #this turns the padded_list in strings based on the tuples created from zipping them, they are already at the correct length
+    tuples_list = ["".join(str(tuple) for tuple in tpl) for tpl in zip(*padded_list)]
+    #this line is the final variable needed it joines together the touples with the f-string formatting into one line, I am getting better at one liners
+    block_of_categories = "".join([f"     {tuple}\n" for tuple in tuples_list])
+
+
 
 
 
@@ -71,42 +80,140 @@ def create_spend_chart(categories = []):
     
     title_line = "Percentage spent by category\n"
     hund_line = "100| "
-    ninty_line = " 90|"
-    eighty_line = " 80|"
-    seventy_line = " 70|"
-    sixty_line = " 60|"
-    fifty_line = " 50|"
-    forty_line = " 40|"
-    thirty_line = " 30|"
-    twenty_line = " 20|"
-    tens_line = " 10|"
-    zero_line = "  0|"
-    divider_line = "-"*(len(categories)*3+4)
+    ninty_line = " 90| "
+    eighty_line = " 80| "
+    seventy_line = " 70| "
+    sixty_line = " 60| "
+    fifty_line = " 50| "
+    forty_line = " 40| "
+    thirty_line = " 30| "
+    twenty_line = " 20| "
+    tens_line = " 10| "
+    zero_line = "  0| "
+    divider_line = f"    {'-'*(len(categories)*3+1)}"
 
     for percentage in percentages:
         if percentage == 100:
             hund_line += "o  "
-        elif 90 <= percentage:
             ninty_line += "o  "
-        elif 80 <= percentage:
             eighty_line += "o  "
-        elif 70 <= percentage:
             seventy_line += "o  "
-        elif 60 <= percentage:
             sixty_line += "o  "
-        elif 50 <= percentage:
             fifty_line += "o  "
-        elif 40 <= percentage:
             forty_line += "o  "
-        elif 30 <= percentage:
             thirty_line += "o  "
-        elif 20 <= percentage:
             twenty_line += "o  "
-        elif 10 <= percentage:
             tens_line += "o  "
-        elif 0 <= percentage:
             zero_line += "o  "
-        else:
+        elif 90 == percentage:
+            hund_line += "   "
+            ninty_line += "o  "
+            eighty_line += "o  "
+            seventy_line += "o  "
+            sixty_line += "o  "
+            fifty_line += "o  "
+            forty_line += "o  "
+            thirty_line += "o  "
+            twenty_line += "o  "
+            tens_line += "o  "
+            zero_line += "o  "
+        elif 80 == percentage:
+            hund_line += "   "
+            ninty_line += "   "
+            eighty_line += "o  "
+            seventy_line += "o  "
+            sixty_line += "o  "
+            fifty_line += "o  "
+            forty_line += "o  "
+            thirty_line += "o  "
+            twenty_line += "o  "
+            tens_line += "o  "
+            zero_line += "o  "
+        elif 70 <= percentage:
+            hund_line += "   "
+            ninty_line += "   "
+            eighty_line += "   "
+            seventy_line += "o  "
+            sixty_line += "o  "
+            fifty_line += "o  "
+            forty_line += "o  "
+            thirty_line += "o  "
+            twenty_line += "o  "
+            tens_line += "o  "
+            zero_line += "o  "
+        elif 60 <= percentage:
+            hund_line += "   "
+            ninty_line += "   "
+            eighty_line += "   "
+            seventy_line += "   "
+            sixty_line += "o  "
+            fifty_line += "o  "
+            forty_line += "o  "
+            thirty_line += "o  "
+            twenty_line += "o  "
+            tens_line += "o  "
+            zero_line += "o  "
+        elif 50 <= percentage:
+            hund_line += "   "
+            ninty_line += "   "
+            eighty_line += "   "
+            seventy_line += "   "
+            sixty_line += "   "
+            fifty_line += "o  "
+            forty_line += "o  "
+            thirty_line += "o  "
+            twenty_line += "o  "
+            tens_line += "o  "
+            zero_line += "o  "
+        elif 40 <= percentage:
+            hund_line += "   "
+            ninty_line += "   "
+            eighty_line += "   "
+            seventy_line += "   "
+            sixty_line += "   "
+            fifty_line += "   "
+            forty_line += "o  "
+            thirty_line += "o  "
+            twenty_line += "o  "
+            tens_line += "o  "
+            zero_line += "o  "
+        elif 30 <= percentage:
+            hund_line += "   "
+            ninty_line += "   "
+            eighty_line += "   "
+            seventy_line += "   "
+            sixty_line += "   "
+            fifty_line += "   "
+            forty_line += "   "
+            thirty_line += "o  "
+            twenty_line += "o  "
+            tens_line += "o  "
+            zero_line += "o  "
+        elif 20 <= percentage:
+            hund_line += "   "
+            ninty_line += "   "
+            eighty_line += "   "
+            seventy_line += "   "
+            sixty_line += "   "
+            fifty_line += "   "
+            forty_line += "   "
+            thirty_line += "   "
+            twenty_line += "o  "
+            tens_line += "o  "
+            zero_line += "o  "
+        elif 10 <= percentage:
+            hund_line += "   "
+            ninty_line += "   "
+            eighty_line += "   "
+            seventy_line += "   "
+            sixty_line += "   "
+            fifty_line += "   "
+            forty_line += "   "
+            thirty_line += "   "
+            twenty_line += "   "
+            tens_line += "o  "
+            zero_line += "o  "
+        elif 0 <= percentage:
             hund_line += "   "
             ninty_line += "   "
             eighty_line += "   "
@@ -117,11 +224,11 @@ def create_spend_chart(categories = []):
             thirty_line += "   "
             twenty_line += "   "
             tens_line += "   "
-            zero_line += "   "
+            zero_line += "o  "
 
-    chart = str(title_line + hund_line + "\n" + ninty_line + "\n" + eighty_line + "\n" + seventy_line + "\n" + sixty_line + "\n" + fifty_line + "\n" + forty_line + "\n" + thirty_line + "\n" + twenty_line + "\n" + tens_line + "\n" + zero_line + "\n" + divider_line + "\n")
+    chart = f"{title_line}{hund_line}\n{ninty_line}\n{eighty_line}\n{seventy_line}\n{sixty_line}\n{fifty_line}\n{forty_line}\n{thirty_line}\n{twenty_line}\n{tens_line}\n{zero_line}\n{divider_line}\n{block_of_categories}"
 
-    return print(equal_letter)
+    return chart
 
 food = Category("Food")
 entertainment = Category("Entertainment")
@@ -132,4 +239,6 @@ business.deposit(900, "deposit")
 food.withdraw(105.55)
 entertainment.withdraw(33.40)
 business.withdraw(10.99)
-create_spend_chart([food, entertainment, business])
+create_spend_chart([business, food, entertainment])
+answer = "Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  "
+print(answer)
